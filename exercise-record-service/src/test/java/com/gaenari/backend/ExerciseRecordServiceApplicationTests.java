6b8bcc212ab177da882e07de032d59447e433d3a
최다희 <dahee121816@gmail.com>
2024-04-23 13:01:30 +0900

    :sparkles: feat : Exercise Record Service 생성

---
+++ exercise-record-service/src/test/java/com/gaenari/backend/ExerciseRecordServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaenari.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciseRecordServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
