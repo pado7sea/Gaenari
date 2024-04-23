891cfe1753539f87387c094f0d86463f0fc87b41
최다희 <1218dahee@naver.com>
2024-04-24 08:59:15 +0900

    :wrench: refact : service 별 브랜치 분할로 인한 파일 삭제
    (cherry picked from commit b0b0d6ae8ab45726ee7330fc0c5bb14cd8e9d848)
    (cherry picked from commit da6c7e5cfd1b0d04118e2eaeff4eac3dbbceac79)

--- member-service/src/main/java/com/gaenari/backend/domain/member/repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gaenari.backend.domain.member.repository;
-
-import com.gaenari.backend.domain.member.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
--- member-service/src/main/java/com/gaenari/backend/domain/member/service/MemberService.java
+++
@@ -1,4 +0,0 @@
-package com.gaenari.backend.domain.member.service;
-
-public interface MemberService {
-}
