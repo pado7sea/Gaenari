e8bd5d083a8225347d07917b40bec1c740d20724
서만기 <seo_m98@naver.com>
2024-04-25 19:19:11 +0900

    :sparkles: feat : 모달 및 앱바 위젯 생성

---
+++ frontend/lib/widgets/modal.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:forsythia/theme/text.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('모달 위젯 예제'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            showModalBottomSheet(
+              context: context,
+              builder: (BuildContext context) {
+                return ModalContent();
+              },
+            );
+          },
+          child: Text('모달 열기'),
+        ),
+      ),
+    );
+  }
+}
+
+class ModalContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 200,
+      padding: EdgeInsets.all(20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  InkWell(
+                      onTap: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Image.asset(
+                        'assets/icons/common_close.png',
+                        width: 20.0,
+                        height: 20.0,
+                      )),
+                  TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Text12(
+                        text: "닫기",
+                      ))
+                ],
+              ),
+              Text(
+                '여기가 모달 위젯이야!',
+                style: TextStyle(fontSize: 20),
+              ),
+            ],
+          ),
+          SizedBox(height: 20),
+          Text('모달 내용'),
+          SizedBox(height: 20),
+        ],
+      ),
+    );
+  }
+}
