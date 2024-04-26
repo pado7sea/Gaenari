640771eff8ab6e5f21727ca5abf8e560700417d9
서만기 <seo_m98@naver.com>
2024-04-26 23:44:11 +0900

    :sparkles: feat : 컴포넌트 위젯 수정

--- frontend/lib/widgets/box.dart
+++ frontend/lib/widgets/box.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:forsythia/theme/color.dart';
 
 final BoxDecoration myBoxDecoration = BoxDecoration(
   color: Color(0xffFFFFFF), // 배경색
@@ -10,6 +11,19 @@ final BoxDecoration myBoxDecoration = BoxDecoration(
       offset: Offset(0, 10), // 그림자의 위치 (x, y)
     ),
   ],
+);
+final BoxDecoration myBorderBoxDecoration = BoxDecoration(
+  color: Color(0xffFFFFFF), // 배경색
+  borderRadius: BorderRadius.circular(15), // 박스의 모서리를 둥글게
+  boxShadow: [
+    BoxShadow(
+      color: Color(0xffBFC2C8).withOpacity(0.25), // 그림자 색 (투명도 30%)
+      // spreadRadius: 2, // 그림자 퍼지는 정도
+      blurRadius: 15, // 그림자 흐림 정도
+      offset: Offset(0, 10), // 그림자의 위치 (x, y)
+    ),
+  ],
+  border: Border.all(color: myLightGreen, width: 1),
 );
 
 final Container myContainer = Container(
