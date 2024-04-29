b7169c72f4ff6ce9d2d4762a5a99e9d8dc1eb0bd
이재신 <jaesin463@gmail.com>
2024-04-29 15:42:03 +0900

    :wrench: refact : RequestMapping 간소화

--- inventory-service/src/main/java/com/gaenari/backend/Health.java
+++ inventory-service/src/main/java/com/gaenari/backend/Health.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/inventory-service")
+@RequestMapping("/")
 public class Health {
     Environment env;
     @Autowired
