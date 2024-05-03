71164dc7e3e13ad3cd1a142f459bbbfdb5977f30
최다희 <dahee121816@gmail.com>
2024-05-03 16:56:36 +0900

    :sparkles: feat : 회원토큰인식

--- member-service/src/main/java/com/gaenari/backend/domain/member/controller/MemberController.java
+++ member-service/src/main/java/com/gaenari/backend/domain/member/controller/MemberController.java
@@ -1,7 +1,6 @@
 package com.gaenari.backend.domain.member.controller;
 
 import com.gaenari.backend.domain.member.dto.requestDto.MemberUpdate;
-import com.gaenari.backend.domain.member.dto.requestDto.RequestLogin;
 import com.gaenari.backend.domain.member.dto.requestDto.SignupRequestDto;
 import com.gaenari.backend.domain.member.dto.responseDto.SignupResponse;
 import com.gaenari.backend.domain.member.service.MemberService;
@@ -10,20 +9,10 @@ import com.gaenari.backend.global.format.code.ErrorCode;
 import com.gaenari.backend.global.format.code.ResponseCode;
 import com.gaenari.backend.global.format.response.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpServletRequest;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -39,7 +28,7 @@ public class MemberController {
                 env.getProperty("local.server.port"));
     }
 
-    @Operation(summary = "회원가입", description = "회원가입")
+    @Operation(summary = "회원가입", description = "아이디 최대 50글자, 닉네임 최대 10글자, 비밀번호 최대 12글자(대소문자구분)")
     @PostMapping("/members") // 회원가입
     public ResponseEntity<?> signup(@RequestBody SignupRequestDto requestDto){
         try {
@@ -52,21 +41,9 @@ public class MemberController {
         }
     }
 
-    @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody RequestLogin requestLogin) {
-        return ResponseEntity.status(HttpStatus.OK).body(requestLogin);
-    }
-
-//    @PostMapping("/logout")
-//    public ResponseEntity<?> logout() {
-//        return ResponseEntity.status(HttpStatus.OK).body("로그아웃 성공");
-//    }
     @Operation(summary = "회원 탈퇴", description = "회원 탈퇴 (삭제처리)")
     @DeleteMapping("/leave")
-    public ResponseEntity<?> withdrawMember() {
-        // 헤더에서 memberEmail 추출
-//        String memberEmail = authentication.getName();
-        String memberEmail = "ssafy123@naver.com";
+    public ResponseEntity<?> withdrawMember(@RequestHeader("User-Info") String memberEmail) {
         // memberId가 null이면 인증 실패
         if (memberEmail == null) {
             return response.error(ErrorCode.EMPTY_MEMBER.getMessage());
@@ -78,10 +55,7 @@ public class MemberController {
 
     @Operation(summary = "회원 닉네임 수정", description = "회원 닉네임 수정")
     @PutMapping("/member/nickname")
-    public ResponseEntity<?> updateNick(@RequestParam String nickName){
-        // 헤더에서 memberEmail 추출
-//        String memberEmail = authentication.getName();
-        String memberEmail = "ssafy123@naver.com";
+    public ResponseEntity<?> updateNick(@RequestHeader("User-Info") String memberEmail, @RequestParam String nickName){
         // memberId가 null이면 인증 실패
         if (memberEmail == null) {
             return response.error(ErrorCode.EMPTY_MEMBER.getMessage());
@@ -91,10 +65,7 @@ public class MemberController {
     }
     @Operation(summary = "회원 비밀번호 수정", description = "회원 비밀번호 수정")
     @PutMapping("/member/password")
-    public ResponseEntity<?> updatePwd(@RequestBody String newPassword){
-        // 헤더에서 memberEmail 추출
-//        String memberEmail = authentication.getName();
-        String memberEmail = "ssafy123@naver.com";
+    public ResponseEntity<?> updatePwd(@RequestHeader("User-Info") String memberEmail, @RequestBody String newPassword){
         // memberId가 null이면 인증 실패
         if (memberEmail == null) {
             return response.error(ErrorCode.EMPTY_MEMBER.getMessage());
@@ -104,10 +75,7 @@ public class MemberController {
     }
     @Operation(summary = "회원 정보 수정", description = "회원 정보 수정")
     @PutMapping("/member/info")
-    public ResponseEntity<?> updateInfo(@RequestBody MemberUpdate memberUpdate){
-        // 헤더에서 memberEmail 추출
-//        String memberEmail = authentication.getName();
-        String memberEmail = "ssafy123@naver.com";
+    public ResponseEntity<?> updateInfo(@RequestHeader("User-Info") String memberEmail, @RequestBody MemberUpdate memberUpdate){
         // memberId가 null이면 인증 실패
         if (memberEmail == null) {
             return response.error(ErrorCode.EMPTY_MEMBER.getMessage());
@@ -118,10 +86,7 @@ public class MemberController {
 
     @Operation(summary = "회원보유코인 조회", description = "회원보유코인 조회")
     @GetMapping("/member/coin")
-    public ResponseEntity<?> getMemberCoin() {
-        // 헤더에서 memberEmail 추출
-//        String memberEmail = authentication.getName();
-        String memberEmail = "ssafy123@naver.com";
+    public ResponseEntity<?> getMemberCoin(@RequestHeader("User-Info") String memberEmail) {
         // memberId가 null이면 인증 실패
         if (memberEmail == null) {
             return response.error(ErrorCode.EMPTY_MEMBER.getMessage());
@@ -131,15 +96,8 @@ public class MemberController {
     }
 
     @Operation(summary = "닉네임 중복체크", description = "닉네임 중복체크")
-    @GetMapping("/member/dupl/nickname")
+    @GetMapping("/dupl/nickname")
     public ResponseEntity<?> duplNickName(@RequestParam String nickName){
-        // 헤더에서 memberEmail 추출
-//        String memberEmail = authentication.getName();
-        String memberEmail = "ssafy123@naver.com";
-        // memberId가 null이면 인증 실패
-        if (memberEmail == null) {
-            return response.error(ErrorCode.EMPTY_MEMBER.getMessage());
-        }
         Boolean isDuplNickName = memberService.duplNickNameCheck(nickName);
         if(isDuplNickName){
             // 사용중
@@ -150,23 +108,16 @@ public class MemberController {
         }
     }
 
-    @Operation(summary = "이메일 중복체크", description = "이메일 중복체크")
-    @GetMapping("/member/dupl/email")
+    @Operation(summary = "아이디 중복체크", description = "아이디 중복체크")
+    @GetMapping("/dupl/email")
     public ResponseEntity<?> duplEmail(@RequestParam String email){
-        // 헤더에서 memberEmail 추출
-//        String memberEmail = authentication.getName();
-        String memberEmail = "ssafy123@naver.com";
-        // memberId가 null이면 인증 실패
-        if (memberEmail == null) {
-            return response.error(ErrorCode.EMPTY_MEMBER.getMessage());
-        }
         Boolean isDuplEmail = memberService.duplEmailCheck(email);
         if(isDuplEmail){
             // true : 사용중
-            return response.success(ResponseCode.ALREADY_USE_EMAIL, isDuplEmail);
+            return ApiResponse.getInstance().success(ResponseCode.ALREADY_USE_EMAIL, isDuplEmail);
         }else{
             // false : 사용가능한
-            return response.success(ResponseCode.AVAILABLE_EMAIL, isDuplEmail);
+            return ApiResponse.getInstance().success(ResponseCode.AVAILABLE_EMAIL, isDuplEmail);
         }
     }
 
