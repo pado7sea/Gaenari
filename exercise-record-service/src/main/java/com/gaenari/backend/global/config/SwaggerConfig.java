05a66ebbbbf8bdc1faf19ba0be1ca35a2e56b786
정혜진 <pado7sea@gmail.com>
2024-05-07 04:45:58 +0900

    :sparkles: feat : swagger config에 jwt 추가

--- exercise-record-service/src/main/java/com/gaenari/backend/global/config/SwaggerConfig.java
+++ exercise-record-service/src/main/java/com/gaenari/backend/global/config/SwaggerConfig.java
@@ -2,7 +2,10 @@ package com.gaenari.backend.global.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -11,15 +14,25 @@ import org.springframework.context.annotation.Configuration;
 @RequiredArgsConstructor
 @Configuration
 @OpenAPIDefinition(
-    info = @Info(title = "API Document", description = "Exercise Record SERVICE 명세서", version = "v3")
+        info = @Info(title = "API Document", description = "Exercise Record SERVICE 명세서", version = "v3")
 )
 public class SwaggerConfig {
 
-  @Bean
-  public OpenAPI openAPI() {
-    return new OpenAPI()
-        .addServersItem(new Server().url("http://localhost:8080/api/exercise-record-service/"))
-        .addServersItem(new Server().url("https://api.gaenari.kr/api/exercise-record-service/"));
-  }
+    @Bean
+    public OpenAPI openAPI() {
+        String jwt = "JWT";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt);
+        Components components = new Components().addSecuritySchemes(jwt, new SecurityScheme()
+                .name(jwt)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+        );
+        return new OpenAPI()
+                .addServersItem(new Server().url("http://localhost:8080/api/exercise-record-service/"))
+                .addServersItem(new Server().url("https://api.gaenari.kr/api/exercise-record-service/"))
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
 
 }
