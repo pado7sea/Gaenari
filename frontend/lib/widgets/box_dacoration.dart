748e4cd7f3d4aa8dde70aa2ee7a171e0b377962a
서만기 <seo_m98@naver.com>
2024-05-06 17:31:48 +0900

    :sparkles: feat : 쓰레기통 아이콘 추가 및 빨간색박스 위젯 추가

--- frontend/lib/widgets/box_dacoration.dart
+++ frontend/lib/widgets/box_dacoration.dart
@@ -12,6 +12,17 @@ final BoxDecoration myBoxDecoration = BoxDecoration(
     ),
   ],
 );
+final BoxDecoration myRedBoxDecoration = BoxDecoration(
+  color: myRed, // 배경색
+  borderRadius: BorderRadius.circular(15), // 박스의 모서리를 둥글게
+  boxShadow: [
+    BoxShadow(
+      color: Color(0xffBFC2C8).withOpacity(0.25), // 그림자 색 (투명도 25%)
+      blurRadius: 15, // 그림자 흐림 정도
+      offset: Offset(0, 10), // 그림자의 위치 (x, y)
+    ),
+  ],
+);
 final BoxDecoration myBorderBoxDecoration = BoxDecoration(
   color: Color(0xffFFFFFF), // 배경색
   borderRadius: BorderRadius.circular(15), // 박스의 모서리를 둥글게
