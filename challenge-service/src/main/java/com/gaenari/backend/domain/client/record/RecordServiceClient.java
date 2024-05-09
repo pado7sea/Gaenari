93f356eca37906bc5e3d75bfe88035010ac99f9a
정혜진 <pado7sea@gmail.com>
2024-05-09 10:38:45 +0900

    :sparkles: feat : challenge-service swagger에서 user-info 파라미터 hidden으로 처리

--- challenge-service/src/main/java/com/gaenari/backend/domain/client/record/RecordServiceClient.java
+++ challenge-service/src/main/java/com/gaenari/backend/domain/client/record/RecordServiceClient.java
@@ -1,15 +1,17 @@
-package com.gaenari.backend.domain.client;
+package com.gaenari.backend.domain.client.record;
 
-import com.gaenari.backend.domain.client.dto.TotalStatisticDto;
+import com.gaenari.backend.domain.client.record.dto.TotalStatisticDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "exercise-record-service")
+@FeignClient(name = "exercise-record-service", url = "${feign.member-service.url}")
 public interface RecordServiceClient {
 
-    // exercise-record-service에서 누적 기록 가져오기
+    // 회원 ID를 보내고 누적 기록 가져오기
     @GetMapping("/statistic/feign/{memberId}")
     TotalStatisticDto getAllStatistics(@PathVariable(name = "memberId") String memberId);
+
+    // 회원 ID와 recordChallenge 리스트를 반환
 
 }
