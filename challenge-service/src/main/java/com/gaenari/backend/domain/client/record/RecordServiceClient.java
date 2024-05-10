e41667ccaefa424016eff9ec74ff0b0313f09840
정혜진 <pado7sea@gmail.com>
2024-05-10 15:56:15 +0900

    :sparkles: feat : 받지 않은 모든 보상 받기, 해당 운동 기록 관련 보상 받기, 받지 않은 보상 여부 조회 API 추가

--- challenge-service/src/main/java/com/gaenari/backend/domain/client/record/RecordServiceClient.java
+++ challenge-service/src/main/java/com/gaenari/backend/domain/client/record/RecordServiceClient.java
@@ -1,17 +1,27 @@
 package com.gaenari.backend.domain.client.record;
 
 import com.gaenari.backend.domain.client.record.dto.TotalStatisticDto;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "exercise-record-service", url = "${feign.member-service.url}")
+import java.util.List;
+
+@FeignClient(name = "exercise-record-service", url = "${feign.exercise-record-service.url}")
 public interface RecordServiceClient {
 
     // 회원 ID를 보내고 누적 기록 가져오기
     @GetMapping("/statistic/feign/{memberId}")
     TotalStatisticDto getAllStatistics(@PathVariable(name = "memberId") String memberId);
 
-    // 회원 ID와 recordChallenge 리스트를 반환
+    // 회원의 도전과제 ID 리스트 조회
+    @GetMapping("/record/feign/recordChallenge/{memberId}")
+    List<Integer> getChallengeIdsByMemberId(@Parameter(name = "회원 ID") @PathVariable(name = "memberId") String memberId);
+
+    // 운동 기록의 도전과제 ID 리스트 조회
+    @GetMapping("/record/feign/recordChallenge/{memberId}/{recordId}")
+    List<Integer> getChallengeIdsByRecordId(@Parameter(name = "회원 ID") @PathVariable(name = "memberId") String memberId,
+                                                   @Parameter(name = "운동 기록 ID") @PathVariable(name = "recordId") Long recordId);
 
 }
--- challenge-service/src/main/java/com/gaenari/backend/domain/reward/service/impl/RewardServiceImpl.java
+++ challenge-service/src/main/java/com/gaenari/backend/domain/reward/service/impl/RewardServiceImpl.java
@@ -4,57 +4,86 @@ import com.gaenari.backend.domain.challenge.dto.enumType.ChallengeCategory;
 import com.gaenari.backend.domain.client.member.MemberServiceClient;
 import com.gaenari.backend.domain.client.member.dto.HeartChangeDto;
 import com.gaenari.backend.domain.client.member.dto.MemberCoinDto;
+import com.gaenari.backend.domain.client.record.RecordServiceClient;
 import com.gaenari.backend.domain.memberChallenge.entity.MemberChallenge;
 import com.gaenari.backend.domain.memberChallenge.repository.MemberChallengeRepository;
 import com.gaenari.backend.domain.reward.dto.RewardDto;
 import com.gaenari.backend.domain.reward.service.RewardService;
-import com.gaenari.backend.global.exception.reward.RewardNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.lang.reflect.Member;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class RewardServiceImpl implements RewardService {
 
+    private final RecordServiceClient recordServiceClient;
     private final MemberServiceClient memberServiceClient;
     private final MemberChallengeRepository memberChallengeRepository;
 
-    // 도전 과제 아이디로 받을 수 있는 보상 찾기
+    // 회원 ID로 보상을 받지 않은 도전과제가 있는지 찾기
     @Override
-    public RewardDto getAttainableReward(String memberId, Integer challengeId) {
-        // 멤버챌린지에서 도전 과제 아이디로 조회
-        MemberChallenge memberChallenge = memberChallengeRepository.findByMemberIdAndChallengeId(memberId, challengeId);
+    public boolean findObtainableChallenge(String memberId) {
+        long count = memberChallengeRepository.findByMemberIdAndObtainableGreaterThan(memberId, 0).stream().count();
+        if (count == 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 
-        // obtainable count 가 0이면 받을 보상이 없음
-        if (memberChallenge.getObtainable() <= 0) {
-            throw new RewardNotFoundException();
+    // 회원 ID로 도전과제 ID 리스트 찾기
+    @Override
+    public List<Integer> getChallengeIdsByMemberId(String memberId) {
+        // 멤버가 가지고 있는 완료한 기록 조회
+        List<MemberChallenge> memberChallenges = memberChallengeRepository.findByMemberIdAndObtainableGreaterThan(memberId, 0);
+        // 도전 과제 ID를 저장할 리스트 생성
+        List<Integer> challengeIds = new ArrayList<>();
+
+        // 완료된 기록들을 순회하면서 도전 과제 ID를 추출하여 리스트에 저장
+        for (MemberChallenge memberChallenge : memberChallenges) {
+            // 도전 과제의 ID를 리스트에 추가
+            challengeIds.add(memberChallenge.getChallenge().getId());
         }
 
+        return challengeIds;
+    }
 
+    // 운동 기록 ID로 운동 기록에 연결되어 있는 도전과제 ID 리스트 찾기
+    @Override
+    public List<Integer> getChallengeIdsByRecordId(String memberId, Long recordId) {
+        // 마이크로 서비스간 통신을 통해 운동 기록 ID로 도전과제 ID 리스트 조회
+        return recordServiceClient.getChallengeIdsByRecordId(memberId, recordId);
+    }
+
+    // 도전 과제 아이디로 받을 수 있는 보상 찾기
+    @Override
+    public RewardDto getAttainableRewardAndReset(String memberId, Integer challengeId) {
+        // 도전 과제 아이디로 모든 멤버 챌린지 조회
+        MemberChallenge memberChallenge = memberChallengeRepository.findByMemberIdAndChallengeId(memberId, challengeId);
+
+        // 도전 과제 아이디로 조회된 멤버 챌린지가 없거나 obtainableCount가 0 이하인 경우
+        if (memberChallenge.getObtainable() <= 0) {
+            return null;
+        }
 
         // 업적 및 미션 카테고리에 따라 얻을 수 있는 코인 및 애정도 계산
         ChallengeCategory category = memberChallenge.getChallenge().getCategory();
 
-        Integer attainableCoin = 0;
-        Integer attainableHeart = 0;
-
-        System.out.println("memberChallengeId"+memberChallenge.getId());
+        int attainableCoin = 0;
+        int attainableHeart = 0;
 
         if (category == ChallengeCategory.TROPHY || category == ChallengeCategory.MISSION) {
             attainableCoin = memberChallenge.getObtainable() * memberChallenge.getChallenge().getCoin();
-            System.out.println("count -> "+memberChallenge.getObtainable());
-            System.out.println("coin -> "+memberChallenge.getChallenge().getCoin());
-
             System.out.println("attainableCoin = " + attainableCoin);
         }
         if (category == ChallengeCategory.MISSION) {
             // 업적일 경우에는 애정도 계산 x
             attainableHeart = memberChallenge.getObtainable() * memberChallenge.getChallenge().getHeart();
             System.out.println("attainableHeart = " + attainableHeart);
-            System.out.println("heart"+memberChallenge.getChallenge().getHeart());
         }
 
         // 받을 수 있는 보상 횟수를 0으로 리셋
@@ -62,17 +91,57 @@ public class RewardServiceImpl implements RewardService {
 
         // 코인과 애정도 반환
         return RewardDto.builder()
-                .challengeId(challengeId)
+                .memberId(memberId)
                 .coin(attainableCoin)
                 .heart(attainableHeart)
                 .build();
     }
+
+    @Override
+    public RewardDto getAttainableRewardAndUpdate(String memberId, Integer challengeId) {
+        // 도전 과제 아이디로 모든 멤버 챌린지 조회
+        MemberChallenge memberChallenge = memberChallengeRepository.findByMemberIdAndChallengeId(memberId, challengeId);
+
+        // 도전 과제 아이디로 조회된 멤버 챌린지가 없거나 obtainableCount가 0 이하인 경우
+        if (memberChallenge.getObtainable() <= 0) {
+            return null;
+        }
+
+        // 업적 및 미션 카테고리에 따라 얻을 수 있는 코인 및 애정도 계산
+        ChallengeCategory category = memberChallenge.getChallenge().getCategory();
+
+        int attainableCoin = 0;
+        int attainableHeart = 0;
+
+        if (category == ChallengeCategory.TROPHY || category == ChallengeCategory.MISSION) {
+            attainableCoin = memberChallenge.getChallenge().getCoin();
+            System.out.println("attainableCoin = " + attainableCoin);
+        }
+        if (category == ChallengeCategory.MISSION) {
+            // 업적일 경우에는 애정도 계산 x
+            attainableHeart = memberChallenge.getChallenge().getHeart();
+            System.out.println("attainableHeart = " + attainableHeart);
+        }
+
+        // 받을 수 있는 보상 횟수를 업데이트
+        updateObtainableCount(memberChallenge, 1);
+
+
+        // 코인과 애정도 반환
+        return RewardDto.builder()
+                .memberId(memberId)
+                .coin(attainableCoin)
+                .heart(attainableHeart)
+                .build();
+    }
+
 
     // 마이크로 서비스 간 통신을 통해서 코인 보상 받게 하기
     @Override
     public void callFeignUpdateCoin(String memberId, Integer coin) {
         MemberCoinDto memberCoinDto = MemberCoinDto.builder()
                 .memberEmail(memberId)
+                .isIncreased(true)
                 .coin(coin)
                 .build();
 
@@ -94,7 +163,15 @@ public class RewardServiceImpl implements RewardService {
         ResponseEntity<?> response = memberServiceClient.updateHeart(heartChangeDto);
     }
 
+    // 도전과제 아이디에 해당하는 멤버챌린지 조회해서 받을 수 있는 보상 개수 리셋하기
+    @Override
+    public void resetObtainableCount(MemberChallenge memberChallenge) {
+        memberChallenge.updateObtainable(0);
+        memberChallengeRepository.save(memberChallenge);
+    }
+
     // 도전과제 아이디에 해당하는 멤버챌린지 조회해서 받을 수 있는 보상 개수 업데이트하기
+    @Override
     public void updateObtainableCount(MemberChallenge memberChallenge, int decreaseValue) {
         ChallengeCategory category = memberChallenge.getChallenge().getCategory();
 
@@ -114,9 +191,5 @@ public class RewardServiceImpl implements RewardService {
         memberChallengeRepository.save(memberChallenge);
     }
 
-    // 도전과제 아이디에 해당하는 멤버챌린지 조회해서 받을 수 있는 보상 개수 리셋하기
-    public void resetObtainableCount(MemberChallenge memberChallenge) {
-        memberChallenge.updateObtainable(0);
-        memberChallengeRepository.save(memberChallenge);
-    }
+
 }
