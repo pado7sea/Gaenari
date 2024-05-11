2c5a7c00c99ab7caacaa3385edb4881a1aeab09d
정혜진 <pado7sea@gmail.com>
2024-05-11 16:30:15 +0900

    :lipstick: style : 컨트롤러에 [Feign] 주석 추가

--- exercise-record-service/src/main/java/com/gaenari/backend/domain/recordFeign/controller/RecordFeignController.java
+++ exercise-record-service/src/main/java/com/gaenari/backend/domain/recordFeign/controller/RecordFeignController.java
@@ -21,13 +21,13 @@ public class RecordFeignController {
 
     private final RecordFeignService recordFeignService;
 
-    @Operation(summary = "프로그램 운동 기록 조회", description = "프로그램 운동 기록 조회")
+    @Operation(summary = "[Feign] 프로그램 운동 기록 조회", description = "프로그램 운동 기록 조회")
     @GetMapping("/{programId}")
     public List<ProgramDetailDto.UsageLogDto> getUsageLog(@Parameter(name = "프로그램 ID") @PathVariable(name = "programId") Long programId) {
         return recordFeignService.getRecordsByProgramId(programId);
     }
 
-    @Operation(summary = "운동 기록의 도전과제 ID 리스트 조회", description = "운동 기록 ID로 도전과제 ID 리스트 반환")
+    @Operation(summary = "[Feign] 운동 기록의 도전과제 ID 리스트 조회", description = "운동 기록 ID로 도전과제 ID 리스트 반환")
     @GetMapping("/recordChallenge/{memberId}/{recordId}")
     public List<Integer> getChallengeIdsByRecordId(@Parameter(name = "회원 ID") @PathVariable(name = "memberId") String memberId,
                                                    @Parameter(name = "운동 기록 ID") @PathVariable(name = "recordId") Long recordId) {
