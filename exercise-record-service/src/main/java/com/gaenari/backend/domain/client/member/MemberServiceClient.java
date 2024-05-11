a46c478ead4d852bcc4d192f5d517dbd454b8aa7
정혜진 <pado7sea@gmail.com>
2024-05-11 13:27:05 +0900

    :sparkles: feat : 일정시간 미접속 시 애정도 하락 로직 추가, 배포환경에서는 자정마다 실행. 로컬에서는 30초마다 실행

--- exercise-record-service/src/main/java/com/gaenari/backend/domain/client/member/MemberServiceClient.java
+++ exercise-record-service/src/main/java/com/gaenari/backend/domain/client/member/MemberServiceClient.java
@@ -1,20 +1,23 @@
 package com.gaenari.backend.domain.client.member;
 
+import com.gaenari.backend.domain.client.member.dto.HeartChangeDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "member-service", url = "${feign.member-service.url}")
 public interface MemberServiceClient {
-
-//    // TODO : 최근 운동기록 날짜 기준으로 일정 기간 지나면 애정도 떨어뜨리는 스케줄 작성. 애정도 감소 요청 보내기
-//    // 파트너 펫 애정도 감소
-//    @PostMapping("/pet/partner/heart/decrease")
-//    Boolean decreaseHeart(@PathVariable(name = "memberId") String memberId);
-
+    
     // 회원 체중 조회
     @GetMapping("/member/weight/{memberEmail}")
     ResponseEntity<?> getWeight(@PathVariable(name = "memberEmail") String memberId);
+
+    // 파트너 펫 애정도 업데이트
+    @PostMapping("/pet/heart/change")
+    ResponseEntity<?> updateHeart(@RequestBody HeartChangeDto heartChangeDto);
+
 
 }
---
+++ exercise-record-service/src/main/java/com/gaenari/backend/domain/scheduler/ScheduledTasks.java
@@ -0,0 +1,44 @@
+package com.gaenari.backend.domain.scheduler;
+
+import com.gaenari.backend.domain.client.member.MemberServiceClient;
+import com.gaenari.backend.domain.client.member.dto.HeartChangeDto;
+import com.gaenari.backend.domain.statistic.entity.Statistic;
+import com.gaenari.backend.domain.statistic.repository.StatisticRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Component
+@AllArgsConstructor
+public class ScheduledTasks {
+
+    private final MemberServiceClient memberServiceClient;
+    private final StatisticRepository statisticRepository;
+
+
+    @Scheduled(cron = "${scheduler.cron.expression}") // 설정 파일에서 정의된 주기(cron.expression)에 따라 실행
+    public void decreaseHeartsForInactiveUsers() {
+
+        int heartDecreaseAmount = 10;
+        int inactivityWeeks = 1;
+
+        // 설정된 기간(inactivity.weeks) 동안 활동하지 않은 사용자들의 ID를 조회하여 그들의 애정도를 감소
+        LocalDateTime oneWeekAgo = LocalDateTime.now().minusWeeks(inactivityWeeks);
+        List<String> inactiveMemberIds = statisticRepository.findByDateBefore(oneWeekAgo)
+                .stream()
+                .map(Statistic::getMemberId)
+                .toList();
+
+        for (String memberId : inactiveMemberIds) {
+            decreaseHeartForMember(memberId, heartDecreaseAmount);
+        }
+    }
+
+    private void decreaseHeartForMember(String memberId, int amount) {
+        memberServiceClient.updateHeart(new HeartChangeDto(memberId, false, amount));
+        System.out.println("Heart decreased for: " + memberId + ", amount: " + amount);
+    }
+}
