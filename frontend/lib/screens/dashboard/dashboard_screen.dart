5361e3a72720fba75b3ef2090a122116e1d71824
서만기 <seo_m98@naver.com>
2024-05-11 14:34:59 +0900

    :sparkles: feat : 변경된 라이브러리 적용

--- frontend/lib/screens/dashboard/dashboard_screen.dart
+++ frontend/lib/screens/dashboard/dashboard_screen.dart
@@ -7,7 +7,7 @@ import 'package:forsythia/theme/color.dart';
 import 'package:forsythia/theme/text.dart';
 import 'package:forsythia/widgets/slide_page_route.dart';
 import 'package:forsythia/widgets/box_dacoration.dart';
-import 'package:draggable_bottom_sheet/draggable_bottom_sheet.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 const List<IconData> icons = [
   Icons.message,
@@ -25,6 +25,7 @@ class DashBoardScreen extends StatefulWidget {
 }
 
 class DashBoardScreenState extends State<DashBoardScreen> {
+  bool a = false;
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<LoginInfo?>(
@@ -37,16 +38,14 @@ class DashBoardScreenState extends State<DashBoardScreen> {
             return Center(child: Text('Error: ${snapshot.error}'));
           } else {
             var nickName = snapshot.data?.nickname ?? 'Unknown';
-            return DraggableBottomSheet(
-              minExtent: 50,
-              useSafeArea: false,
-              curve: Curves.easeIn,
-              barrierColor: Colors.transparent,
-              previewWidget: _previewWidget(),
-              expandedWidget: _expandedWidget(),
-              backgroundWidget: _backgroundWidget(nickName),
-              maxExtent: MediaQuery.of(context).size.height * 0.76,
-              onDragging: (pos) {},
+            return SlidingUpPanel(
+              panel: _panelWidget(),
+              collapsed: _collapsed(),
+              body: _backgroundWidget(nickName),
+              minHeight: a ? 150 : 50,
+              maxHeight: MediaQuery.of(context).size.height - 270,
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20), topRight: Radius.circular(20)),
             );
           }
         }
@@ -54,72 +53,18 @@ class DashBoardScreenState extends State<DashBoardScreen> {
     );
   }
 
-  Widget _previewWidget() {
+  Widget _panelWidget() {
     return Container(
       padding: const EdgeInsets.only(top: 16),
       decoration: const BoxDecoration(
         color: myBackground,
         borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(20),
-          topRight: Radius.circular(20),
-        ),
+            topLeft: Radius.circular(20), topRight: Radius.circular(20)),
       ),
-      width: double.infinity,
       child: Column(
         children: <Widget>[
           Container(
-            width: 40,
-            height: 6,
-            decoration: BoxDecoration(
-              color: const Color.fromARGB(255, 95, 95, 95),
-              borderRadius: BorderRadius.circular(10),
-            ),
-          ),
-          const SizedBox(height: 8),
-        ],
-      ),
-    );
-  }
-
-  Widget _backgroundWidget(String? nickName) {
-    return Scaffold(
-      body: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/images/dashBoardImg.png'),
-              fit: BoxFit.cover, // 이미지를 화면에 맞게 확장
-            ),
-          ),
-          child: SizedBox(
-            height: double.infinity,
-            width: double.infinity,
-            child: Container(
-              margin: EdgeInsets.fromLTRB(20, 50, 0, 0),
-              child: Text20(
-                // text: ' ${Provider.of<TokenProvider>(context).token}',
-                text: '$nickName님! 반가워요! 운동시작해볼까요?',
-                bold: true,
-              ),
-            ),
-          )),
-    );
-  }
-
-  Widget _expandedWidget() {
-    return Container(
-      padding: const EdgeInsets.only(top: 16, bottom: 0),
-      decoration: const BoxDecoration(
-        color: myBackground,
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(20),
-          topRight: Radius.circular(20),
-        ),
-      ),
-      width: double.infinity,
-      child: Column(
-        children: <Widget>[
-          Container(
-            width: 40,
+            width: 60,
             height: 6,
             decoration: BoxDecoration(
               color: myGrey,
@@ -127,105 +72,150 @@ class DashBoardScreenState extends State<DashBoardScreen> {
             ),
           ),
           const SizedBox(height: 8),
-          Expanded(
-            child: GridView(
-              physics: const NeverScrollableScrollPhysics(),
-              padding: EdgeInsets.only(top: 0),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 1,
-                crossAxisSpacing: 10,
-                childAspectRatio: (25 / 360),
-              ),
+          Container(
+            padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
+            color: myBackground,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Container(
-                  padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
-                  color: myBackground,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        height: 60,
-                        width: double.infinity,
-                        padding: EdgeInsets.all(10),
-                        margin: EdgeInsets.only(bottom: 16),
-                        decoration: myBoxDecoration,
-                        child: Text16(
-                          text: "아직 받지않은 보상이 있다요",
-                        ),
-                      ),
-                      Container(
-                        height: 200,
-                        width: double.infinity,
-                        padding: EdgeInsets.all(10),
-                        margin: EdgeInsets.only(bottom: 16),
-                        decoration: myBoxDecoration,
-                        child: Text16(
-                          text: "주간기록",
-                        ),
-                      ),
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                  height: 200,
+                  width: double.infinity,
+                  padding: EdgeInsets.all(10),
+                  margin: EdgeInsets.only(bottom: 16),
+                  decoration: myBoxDecoration,
+                  child: Text16(
+                    text: "주간기록",
+                  ),
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      flex: 5,
+                      child: Column(
                         children: [
-                          Expanded(
-                            flex: 5,
-                            child: Column(
-                              children: [
-                                GestureDetector(
-                                  onTap: () {
-                                    Navigator.of(context).push(SlidePageRoute(
-                                        nextPage: ChallengePage()));
-                                  },
-                                  child: Container(
-                                    height: 120,
-                                    width: double.infinity,
-                                    padding: EdgeInsets.all(10),
-                                    margin: EdgeInsets.only(bottom: 16),
-                                    decoration: myBoxDecoration,
-                                    child: Text16(
-                                      text: "도전과제",
-                                    ),
-                                  ),
-                                ),
-                                GestureDetector(
-                                  onTap: () {
-                                    Navigator.of(context).push(
-                                        SlidePageRoute(nextPage: MatePage()));
-                                  },
-                                  child: Container(
-                                    height: 120,
-                                    width: double.infinity,
-                                    padding: EdgeInsets.all(10),
-                                    // margin: EdgeInsets.only(bottom: 10),
-                                    decoration: myBoxDecoration,
-                                    child: Text16(
-                                      text: "친구",
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          Expanded(
-                            flex: 6,
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.of(context).push(
+                                  SlidePageRoute(nextPage: ChallengePage()));
+                            },
                             child: Container(
-                              height: 256,
-                              padding: EdgeInsets.fromLTRB(10, 10, 10, 40),
-                              margin: EdgeInsets.only(left: 16),
+                              height: 120,
+                              width: double.infinity,
+                              padding: EdgeInsets.all(10),
+                              margin: EdgeInsets.only(bottom: 16),
                               decoration: myBoxDecoration,
                               child: Text16(
-                                text: "워치 or 날씨",
+                                text: "도전과제",
+                              ),
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.of(context)
+                                  .push(SlidePageRoute(nextPage: MatePage()));
+                            },
+                            child: Container(
+                              height: 120,
+                              width: double.infinity,
+                              padding: EdgeInsets.all(10),
+                              // margin: EdgeInsets.only(bottom: 10),
+                              decoration: myBoxDecoration,
+                              child: Text16(
+                                text: "친구",
                               ),
                             ),
                           ),
                         ],
                       ),
-                    ],
-                  ),
+                    ),
+                    Expanded(
+                      flex: 6,
+                      child: Container(
+                        height: 256,
+                        padding: EdgeInsets.fromLTRB(10, 10, 10, 40),
+                        margin: EdgeInsets.only(left: 16),
+                        decoration: myBoxDecoration,
+                        child: Text16(
+                          text: "워치 or 날씨",
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
-          )
+          ),
         ],
+      ),
+    );
+  }
+
+  Widget _collapsed() {
+    return Container(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+          color: myBackground,
+        ),
+        child: Column(
+          children: [
+            const SizedBox(height: 16),
+            Container(
+              width: 60,
+              height: 6,
+              decoration: BoxDecoration(
+                color: myGrey,
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            const SizedBox(height: 8),
+            Text12(
+              text: "스크롤을 올려 대시보드를 확인하세요",
+              textColor: myGrey,
+            ),
+            SizedBox(height: a ? 16 : 0),
+            a
+                ? Container(
+                    height: 60,
+                    width: double.infinity,
+                    padding: EdgeInsets.all(10),
+                    margin: EdgeInsets.only(bottom: 16),
+                    decoration: myBoxDecoration,
+                    child: Text16(
+                      text: "아직 받지않은 보상이 있다요",
+                    ),
+                  )
+                : SizedBox(
+                    height: 0,
+                  ),
+          ],
+        ));
+  }
+
+  Widget _backgroundWidget(String? nickName) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/images/dashBoardImg.png'),
+          fit: BoxFit.cover, // 이미지를 화면에 맞게 확장
+        ),
+        borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+      ),
+      child: SizedBox(
+        height: double.infinity,
+        width: double.infinity,
+        child: Container(
+          margin: EdgeInsets.fromLTRB(20, 50, 0, 0),
+          child: Text20(
+            // text: ' ${Provider.of<TokenProvider>(context).token}',
+            text: '$nickName님, 반가워요! \n오늘도 운동 시작해 볼까요?',
+            bold: true,
+          ),
+        ),
       ),
     );
   }
