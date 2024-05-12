742856eda63c0015b60f0cdff4a9d36f2d81fb8f
정혜진 <pado7sea@gmail.com>
2024-05-13 00:34:52 +0900

    :sparkles: feat : ResponseEntity타입으로 반환받고 제네릭리스폰스 추가

--- program-service/src/main/java/com/gaenari/backend/domain/programFeign/service/ProgramFeignService.java
+++ program-service/src/main/java/com/gaenari/backend/domain/programFeign/service/ProgramFeignService.java
@@ -1,15 +1,11 @@
 package com.gaenari.backend.domain.programFeign.service;
 
-import com.gaenari.backend.domain.program.dto.requestDto.ProgramCreateDto;
-import com.gaenari.backend.domain.program.dto.responseDto.ProgramDetailDto;
-import com.gaenari.backend.domain.program.dto.responseDto.ProgramDto;
 import com.gaenari.backend.domain.programFeign.dto.ProgramDetailAboutRecordDto;
-
-import java.util.List;
 
 public interface ProgramFeignService {
 
     ProgramDetailAboutRecordDto getProgramDetailById(Long programId);
 
     Integer updateProgramUsageCount(Long programId);
+
 }
