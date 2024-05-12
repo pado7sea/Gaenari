5c20f3785fd1b621e36b41f89028059beaefae37
서민지 <s__min_ji@naver.com>
2024-05-12 23:23:41 +0900

    :sparkles: feat : 코인내역 screen UI 제작

--- frontend/lib/screens/doghouse/doghouse_screen.dart
+++ frontend/lib/screens/doghouse/doghouse_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:forsythia/screens/coin/coin_screen.dart';
+import 'package:forsythia/widgets/slide_page_route.dart';
+import 'package:go_router/go_router.dart';
 
 class DogHouseScreen extends StatefulWidget {
   const DogHouseScreen({super.key});
@@ -11,8 +14,14 @@ class _DogHouseScreenState extends State<DogHouseScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      title: Text('강아지집'),
-    ));
+      appBar: AppBar(
+        title: Text('강아지집'),
+      ),
+      body: ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).push(SlidePageRoute(nextPage: CoinScreen()));
+          },
+          child: Text('코인')),
+    );
   }
 }
