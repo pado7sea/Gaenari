d9641c658bbd3c5bb8f372c947d034c129eb6689
정혜진 <pado7sea@gmail.com>
2024-05-13 00:12:04 +0900

    :sparkles: feat : 예외 처리 추가

--- exercise-record-service/src/main/java/com/gaenari/backend/global/format/code/ResponseCode.java
+++ exercise-record-service/src/main/java/com/gaenari/backend/global/format/code/ResponseCode.java
@@ -13,10 +13,12 @@ public enum ResponseCode {
 
     /* 기록(Record) */
     RECORD_ALL_FETCHED(HttpStatus.OK, "전체 기록이 성공적으로 조회되었습니다."),
+    RECORD_PROGRAM_FETCHED(HttpStatus.OK, "프로그램의 운동 기록이 성공적으로 조회되었습니다."),
     RECORD_MONTH_FETCHED(HttpStatus.OK, "월간 기록이 성공적으로 조회되었습니다."),
     RECORD_WEEK_FETCHED(HttpStatus.OK, "주간 기록이 성공적으로 조회되었습니다."),
     RECORD_DAY_FETCHED(HttpStatus.OK, "일일 기록이 성공적으로 조회되었습니다."),
     RECORD_DETAIL_FETCHED(HttpStatus.OK, "기록 상세가 성공적으로 조회되었습니다."),
+    RECORD_CHALLENGE_FETCHED(HttpStatus.OK, "운동 기록의 도전과제 ID 리스트가 성공적으로 조회되었습니다."),
 
     /* 통계 */
     STATISTIC_ALL_FETCHED(HttpStatus.OK, "전체 통계가 성공적으로 조회되었습니다."),
