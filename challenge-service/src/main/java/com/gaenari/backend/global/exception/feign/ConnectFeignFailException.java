61a8045d0aeab5857e3b430962f18105e2d04c5b
정혜진 <pado7sea@gmail.com>
2024-05-13 00:23:08 +0900

    :sparkles: feat : 예외 처리 추가

---
+++ challenge-service/src/main/java/com/gaenari/backend/global/exception/feign/ConnectFeignFailException.java
@@ -0,0 +1,13 @@
+package com.gaenari.backend.global.exception.feign;
+
+import com.gaenari.backend.global.format.code.ErrorCode;
+import lombok.Getter;
+
+@Getter
+public class ConnectFeignFailException extends RuntimeException {
+    private final ErrorCode errorCode;
+
+    public ConnectFeignFailException() {
+        this.errorCode = ErrorCode.CONNECT_FEIGN_FAIL;
+    }
+}
