8304d73c611847afaaac43503345fa9bd8c47b9f
정혜진 <pado7sea@gmail.com>
2024-05-13 00:34:09 +0900

    :wrench: refact : ProgramService와 FavoriteService에서 중복되는 코드를 ProgramBaseService를 만들어 하나로 처리

--- program-service/src/main/java/com/gaenari/backend/domain/program/repository/ProgramRepository.java
+++ program-service/src/main/java/com/gaenari/backend/domain/program/repository/ProgramRepository.java
@@ -3,12 +3,16 @@ package com.gaenari.backend.domain.program.repository;
 import com.gaenari.backend.domain.program.entity.Program;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.swing.*;
 import java.util.List;
+import java.util.Optional;
 
 public interface ProgramRepository extends JpaRepository<Program, Long> {
 
+    // 해당 회원의 프로그램 리스트를 즐겨찾기, 사용횟수 순으로 정렬
     List<Program> findByMemberIdOrderByIsFavoriteDescUsageCountDesc(String programId);
 
+    // 해당 회원이 즐겨찾기한 프로그램 리스트를 사용횟수 순으로 정렬
+    List<Program> findByMemberIdAndIsFavoriteOrderByUsageCountDesc(String memberId, Boolean isFavorite);
 
+    Program findByMemberIdAndId(String memberId, Long programId);
 }
