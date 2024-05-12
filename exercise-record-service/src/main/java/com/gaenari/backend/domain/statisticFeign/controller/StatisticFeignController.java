2ee077ac5ca135dc01ccd8efb269d24723a5e530
정혜진 <pado7sea@gmail.com>
2024-05-13 00:14:05 +0900

    :sparkles: feat : feign 리스폰스 타입을 ResponseEntity로 바꾸고 제네릭 리스폰스 추가

--- exercise-record-service/src/main/java/com/gaenari/backend/domain/statisticFeign/controller/StatisticFeignController.java
+++ exercise-record-service/src/main/java/com/gaenari/backend/domain/statisticFeign/controller/StatisticFeignController.java
@@ -2,10 +2,13 @@ package com.gaenari.backend.domain.statisticFeign.controller;
 
 import com.gaenari.backend.domain.statistic.dto.responseDto.TotalStatisticDto;
 import com.gaenari.backend.domain.statistic.service.StatisticService;
+import com.gaenari.backend.global.format.code.ResponseCode;
+import com.gaenari.backend.global.format.response.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,14 +20,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/statistic/feign")
 public class StatisticFeignController {
 
+    ApiResponse response;
     private final StatisticService statisticService;
 
     @Operation(summary = "[Feign] 전체 통계 조회", description = "저장되어있는 누적값 조회")
     @GetMapping("/{memberId}")
-    public TotalStatisticDto getTotalStatistics(@Parameter(name = "회원 ID")  @PathVariable(name = "memberId") String memberId) {
+    public ResponseEntity<?> getTotalStatistics(@Parameter(description = "회원 ID") @PathVariable(name = "memberId") String memberId) {
         TotalStatisticDto statistics = statisticService.getTotalStatistics(memberId);
 
-        return statistics;
+        return response.success(ResponseCode.STATISTIC_ALL_FETCHED, statistics);
     }
 
 }
