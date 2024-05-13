f7ed80aa5a34807e33b743a7a3272308f2bb0041
서만기 <seo_m98@naver.com>
2024-05-13 15:44:39 +0900

    :sparkles: feat : 보관함 작업중

---
+++ frontend/lib/screens/inventory/inventory_info.dart
@@ -0,0 +1,112 @@
+import 'package:forsythia/models/inventory/item_info.dart';
+
+List<ItemInfo> info = [
+  ItemInfo.fromJson({
+    "setId": 1,
+    "setName": "기본",
+    "item": [
+      {"itemId": 1, "itemName": "벽지", "category": "Wall"},
+      {"itemId": 2, "itemName": "바닥", "category": "Floor"},
+      {"itemId": 3, "itemName": "러그", "category": "Rug"},
+      {"itemId": 4, "itemName": "밥그릇", "category": "Bowl"},
+      {"itemId": 5, "itemName": "방석", "category": "Cushion"},
+      {"itemId": 6, "itemName": "곰도링", "category": "Toy"}
+    ]
+  }),
+  ItemInfo.fromJson({
+    "setId": 2,
+    "setName": "교실",
+    "item": [
+      {"itemId": 7, "itemName": "교실 창문있고 그런 배경", "category": "Wall"},
+      {"itemId": 8, "itemName": "교실바닥", "category": "Floor"},
+      {"itemId": 9, "itemName": "대충 칠판색깔 러그", "category": "Rug"},
+      {"itemId": 10, "itemName": "급식판 (밥그릇)", "category": "Bowl"},
+      {"itemId": 11, "itemName": "책가방", "category": "Cushion"},
+      {"itemId": 12, "itemName": "삼선슬리퍼", "category": "Toy"}
+    ]
+  }),
+  ItemInfo.fromJson({
+    "setId": 3,
+    "setName": "도시",
+    "item": [
+      {
+        "itemId": 13,
+        "itemName": "회색건물 칙칙하게(건물들 아파트들 막 많음)",
+        "category": "Wall"
+      },
+      {
+        "itemId": 14,
+        "itemName": "아스팔트바닥 (막 노란색 중앙선이랑 흰색 도로 경계선 있음, 그리고인도도 조금 보임)",
+        "category": "Floor"
+      },
+      {
+        "itemId": 15,
+        "itemName": "파지 신문지 러그 (박스 접어둔 그런 모습임, 신문지도 살짝 보임)",
+        "category": "Rug"
+      },
+      {"itemId": 16, "itemName": "통조림", "category": "Bowl"},
+      {"itemId": 17, "itemName": "주황색 꼬깔", "category": "Cushion"},
+      {"itemId": 18, "itemName": "테니스공", "category": "Toy"}
+    ]
+  }),
+  ItemInfo.fromJson({
+    "setId": 4,
+    "setName": "아늑한집",
+    "item": [
+      {"itemId": 19, "itemName": "핑크, 아이보리 섞인 벽지", "category": "Wall"},
+      {"itemId": 20, "itemName": "자작나무바닥", "category": "Floor"},
+      {"itemId": 21, "itemName": "핑크러그", "category": "Rug"},
+      {"itemId": 22, "itemName": "예쁜 밥그릇", "category": "Bowl"},
+      {"itemId": 23, "itemName": "소파", "category": "Cushion"},
+      {"itemId": 24, "itemName": "개껌", "category": "Toy"}
+    ]
+  }),
+  ItemInfo.fromJson({
+    "setId": 5,
+    "setName": "카페",
+    "item": [
+      {"itemId": 25, "itemName": "카페느낌나는 배경", "category": "Wall"},
+      {"itemId": 26, "itemName": "찐한 갈색 바닥", "category": "Floor"},
+      {"itemId": 27, "itemName": "어서오세요 발매트", "category": "Rug"},
+      {"itemId": 28, "itemName": "케이크 접시", "category": "Bowl"},
+      {"itemId": 29, "itemName": "쿠션", "category": "Cushion"},
+      {"itemId": 30, "itemName": "에스프레소 머신", "category": "Toy"}
+    ]
+  }),
+  ItemInfo.fromJson({
+    "setId": 6,
+    "setName": "성심당",
+    "item": [
+      {"itemId": 31, "itemName": "성심당 배경", "category": "Wall"},
+      {"itemId": 32, "itemName": "적당한 갈색 마루바닥", "category": "Floor"},
+      {"itemId": 33, "itemName": "적당히 이쁜 러그", "category": "Rug"},
+      {"itemId": 34, "itemName": "망고시루", "category": "Bowl"},
+      {"itemId": 35, "itemName": "빵(튀소)", "category": "Cushion"},
+      {"itemId": 36, "itemName": "빵(명바)", "category": "Toy"}
+    ]
+  }),
+  ItemInfo.fromJson({
+    "setId": 7,
+    "setName": "엑스포다리",
+    "item": [
+      {"itemId": 37, "itemName": "엑스포다리 배경", "category": "Wall"},
+      {"itemId": 38, "itemName": "강이 흐르는 바닥", "category": "Floor"},
+      {"itemId": 39, "itemName": "돗자리", "category": "Rug"},
+      {"itemId": 40, "itemName": "밥그릇", "category": "Bowl"},
+      {"itemId": 41, "itemName": "자전거", "category": "Cushion"},
+      {"itemId": 42, "itemName": "꿈돌이", "category": "Toy"}
+    ]
+  }),
+  ItemInfo.fromJson({
+    "setId": 8,
+    "setName": "개나리테마",
+    "item": [
+      {"itemId": 43, "itemName": "하늘하늘한 하늘 (벽지)", "category": "Wall"},
+      {"itemId": 44, "itemName": "잔디바닥 (바닥)", "category": "Floor"},
+      {"itemId": 45, "itemName": "피크닉돗자리 (러그)", "category": "Rug"},
+      {"itemId": 46, "itemName": "피그닉상자 (아이템)", "category": "Bowl"},
+      {"itemId": 47, "itemName": "개나리꽃화분 (아이템)", "category": "Cushion"},
+      {"itemId": 48, "itemName": "다람쥐", "category": "Toy"}
+    ]
+  })
+];
--- frontend/lib/screens/inventory/inventory_screen.dart
+++ frontend/lib/screens/inventory/inventory_screen.dart
@@ -1,6 +1,12 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:forsythia/models/inventory/item_info.dart';
+import 'package:forsythia/models/inventory/set_list.dart';
+import 'package:forsythia/service/inventory_service.dart';
+import 'package:forsythia/theme/color.dart';
+import 'package:forsythia/theme/text.dart';
+import 'package:forsythia/widgets/box_dacoration.dart';
 import 'package:forsythia/widgets/large_app_bar.dart';
+import 'inventory_info.dart';
 
 class InventoryScreen extends StatefulWidget {
   const InventoryScreen({super.key});
@@ -10,10 +16,86 @@ class InventoryScreen extends StatefulWidget {
 }
 
 class _InventoryScreenState extends State<InventoryScreen> {
+  List<Set> set = [];
+  @override
+  void initState() {
+    super.initState();
+    _getSet();
+  }
+
+  _getSet() async {
+    SetList setList = await InventoryService.fetchSetList();
+    setState(() {
+      set = setList.data!;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: LargeAppBar(title: '보관함', sentence: '강아지 집을 꾸며보자!'),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              _buildRowOfItems(0),
+              SizedBox(height: 16), // 행 사이 간격 조절
+              _buildRowOfItems(2),
+              SizedBox(height: 16), // 행 사이 간격 조절
+              _buildRowOfItems(4),
+              SizedBox(height: 16), // 행 사이 간격 조절
+              _buildRowOfItems(6),
+              SizedBox(height: 16), // 행 사이 간격 조절
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildRowOfItems(int startIndex) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly, // 아이템을 가운데 정렬
+      children: [
+        Flexible(flex: 1, child: _buildItemTile(info[startIndex], startIndex)),
+        SizedBox(width: 16),
+        Flexible(
+            flex: 1,
+            child: _buildItemTile(info[startIndex + 1], startIndex + 1)),
+      ],
+    );
+  }
+
+  Widget _buildItemTile(ItemInfo itemInfo, int index) {
+    return InkWell(
+      onTap: () {
+        // 각 아이템을 눌렀을 때의 동작 추가 가능
+      },
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            decoration: myBorderBoxDecoration,
+            height: 160,
+          ),
+          SizedBox(height: 8.0), // 아이템 이름과 세트 이름 사이 간격 조정
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text16(
+                text: "  ${itemInfo.setName!}",
+                bold: true,
+              ),
+              Text16(
+                text: "${set[index].itemCnt.toString()}/6  ",
+                textColor: myGrey,
+              )
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
