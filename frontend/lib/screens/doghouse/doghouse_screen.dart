72972c4683d709a92cb4d37d4b5d396a4930177d
서만기 <seo_m98@naver.com>
2024-05-15 01:45:44 +0900

    :sparkles: feat : 강아지집 제작

--- frontend/lib/screens/doghouse/doghouse_screen.dart
+++ frontend/lib/screens/doghouse/doghouse_screen.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:forsythia/screens/coin/coin_screen.dart';
+import 'package:forsythia/models/inventory/my_item.dart';
+import 'package:forsythia/models/users/login_user.dart';
 import 'package:forsythia/screens/inventory/inventory_screen.dart';
 import 'package:forsythia/screens/item/item_screen.dart';
+import 'package:forsythia/service/inventory_service.dart';
+import 'package:forsythia/service/secure_storage_service.dart';
 import 'package:forsythia/theme/text.dart';
+import 'package:forsythia/widgets/box_dacoration.dart';
 import 'package:forsythia/widgets/dog_app_bar.dart';
 import 'package:forsythia/widgets/slide_page_route.dart';
-import 'package:go_router/go_router.dart';
 
 class DogHouseScreen extends StatefulWidget {
   const DogHouseScreen({super.key});
@@ -15,190 +18,188 @@ class DogHouseScreen extends StatefulWidget {
 }
 
 class _DogHouseScreenState extends State<DogHouseScreen> {
-  bool zoomin = false;
-  bool isToggled = false;
+  My my = My();
+  int coin = 0;
+  int affection = 0;
+  String tier = "";
+  String name = "";
+  bool active = false;
+
+  @override
+  void initState() {
+    super.initState();
+    getItem();
+    loadCoin();
+  }
+
+  getItem() async {
+    MyItem myItem = await InventoryService.fetchMyItem();
+    setState(() {
+      my = myItem.data!;
+      active = true;
+    });
+  }
+
+  Future<void> loadCoin() async {
+    SecureStorageService storageService = SecureStorageService();
+    LoginInfo? info = await storageService.getLoginInfo();
+    setState(() {
+      coin = info?.coin ?? 0;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: zoomin ? null : DogAppBar(),
-        body: zoomin
-            ? Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Column(
-                  children: [
-                    SizedBox(height: 35),
-                    _zoom(),
-                    SizedBox(height: 10),
-                    _togglebutton(),
-                  ],
-                ),
-              )
-            : Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(height: 5),
-                    _zoom(),
-                    SizedBox(height: 10),
-                    _togglebutton(),
-                  ],
-                ),
-              ));
-  }
-
-  Widget _zoom() {
-    return GestureDetector(
-      onTap: () {
-        setState(() {
-          zoomin = !zoomin;
-        });
-      },
-      child: Container(
-        width: 50,
-        height: 50,
-        child: Center(
-            child: zoomin
-                ? Image(
-                    image: AssetImage('assets/icons/zoomout.png'),
-                    width: 18,
-                    height: 18,
-                    fit: BoxFit.cover,
-                    filterQuality: FilterQuality.none,
-                  )
-                : Image(
-                    image: AssetImage('assets/icons/zoomin.png'),
-                    width: 18,
-                    height: 18,
-                    fit: BoxFit.cover,
-                    filterQuality: FilterQuality.none,
-                  )),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          shape: BoxShape.circle,
-          boxShadow: [
-            BoxShadow(
-              color: Color(0xffBFC2C8).withOpacity(0.25), // 그림자 색 (투명도 30%)
-              // spreadRadius: 2, // 그림자 퍼지는 정도
-              blurRadius: 15, // 그림자 흐림 정도
-              offset: Offset(0, 10), // 그림자의 위치 (x, y)
+    return active
+        ? Scaffold(
+            appBar: DogAppBar(
+              tier: my.pet!.tier,
+              name: my.pet!.name,
+              affection: my.pet!.affection,
+              coinvalue: coin,
             ),
-          ],
-        ),
-      ),
-    );
+            body: SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              child: Stack(
+                children: [
+                  Positioned(
+                    left: 0,
+                    top: 0,
+                    child: Image.asset(
+                      "assets/item/1.png",
+                      width: MediaQuery.of(context).size.width,
+                      filterQuality: FilterQuality.none,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Positioned(
+                    left: 0,
+                    bottom: 0,
+                    child: Image.asset(
+                      "assets/item/2.png",
+                      width: MediaQuery.of(context).size.width,
+                      filterQuality: FilterQuality.none,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Positioned(
+                    left: 30,
+                    bottom: 300,
+                    child: Image.asset(
+                      "assets/item/3.png",
+                      width: 250,
+                      filterQuality: FilterQuality.none,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Positioned(
+                    right: 40,
+                    bottom: 200,
+                    child: Image.asset(
+                      "assets/item/4.png",
+                      width: 100,
+                      filterQuality: FilterQuality.none,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Positioned(
+                    left: 40,
+                    bottom: 80,
+                    child: Image.asset(
+                      "assets/item/5.png",
+                      width: 200,
+                      filterQuality: FilterQuality.none,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Positioned(
+                    right: 40,
+                    bottom: 40,
+                    child: Image.asset(
+                      "assets/item/6.png",
+                      width: 80,
+                      filterQuality: FilterQuality.none,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(height: 10),
+                        _togglebutton(),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ))
+        : Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
   }
 
   Widget _togglebutton() {
-    return GestureDetector(
-      onTap: () {
-        setState(() {
-          isToggled = !isToggled;
-        });
-      },
-      child: isToggled
-          ? Container(
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left: 6),
-                      child: Image(
-                        image: AssetImage('assets/icons/common_up.png'),
-                        width: 18,
-                        height: 18,
-                        fit: BoxFit.cover,
-                        filterQuality: FilterQuality.none,
-                      ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          decoration: myBoxDecoration,
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: GestureDetector(
+                onTap: () {
+                  Navigator.of(context)
+                      .push(SlidePageRoute(nextPage: InventoryScreen()))
+                      .then((value) => getItem());
+                },
+                child: Row(
+                  mainAxisSize:
+                      MainAxisSize.min, // Row의 가로 길이를 내부 요소만큼만 차지하도록 설정
+                  children: const [
+                    Image(
+                      image: AssetImage('assets/icons/doghouse_drawer.png'),
+                      width: 30,
+                      height: 30,
+                      fit: BoxFit.cover,
+                      filterQuality: FilterQuality.none,
                     ),
-                    SizedBox(height: 20),
-                    GestureDetector(
-                        onTap: () {
-                          Navigator.of(context).push(
-                              SlidePageRoute(nextPage: InventoryScreen()));
-                        },
-                        child: Row(
-                          mainAxisSize:
-                              MainAxisSize.min, // Row의 가로 길이를 내부 요소만큼만 차지하도록 설정
-                          children: [
-                            Image(
-                              image: AssetImage(
-                                  'assets/icons/doghouse_drawer.png'),
-                              width: 30,
-                              height: 30,
-                              fit: BoxFit.cover,
-                              filterQuality: FilterQuality.none,
-                            ),
-                            Text16(text: ' 보관함   ')
-                          ],
-                        )),
-                    SizedBox(height: 15),
-                    GestureDetector(
-                        onTap: () {
-                          Navigator.of(context)
-                              .push(SlidePageRoute(nextPage: ItemScreen()));
-                        },
-                        child: Row(
-                          mainAxisSize:
-                              MainAxisSize.min, // Row의 가로 길이를 내부 요소만큼만 차지하도록 설정
-                          children: [
-                            Image(
-                              image: AssetImage(
-                                  'assets/icons/doghouse_random.png'),
-                              width: 30,
-                              height: 30,
-                              fit: BoxFit.cover,
-                              filterQuality: FilterQuality.none,
-                            ),
-                            Text16(text: ' 뽑기   ')
-                          ],
-                        )),
-                    SizedBox(height: 5),
+                    SizedBox(width: 5),
+                    Text16(text: '보관함')
                   ],
-                ),
-              ),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(15),
-                boxShadow: [
-                  BoxShadow(
-                    color:
-                        Color(0xffBFC2C8).withOpacity(0.25), // 그림자 색 (투명도 30%)
-                    // spreadRadius: 2, // 그림자 퍼지는 정도
-                    blurRadius: 15, // 그림자 흐림 정도
-                    offset: Offset(0, 10), // 그림자의 위치 (x, y)
-                  ),
-                ],
-              ),
-            )
-          : Container(
-              width: 50,
-              height: 50,
-              child: Center(
-                  child: Image(
-                image: AssetImage('assets/icons/common_down.png'),
-                width: 18,
-                height: 18,
-                fit: BoxFit.cover,
-                filterQuality: FilterQuality.none,
-              )),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                shape: BoxShape.circle,
-                boxShadow: [
-                  BoxShadow(
-                    color:
-                        Color(0xffBFC2C8).withOpacity(0.25), // 그림자 색 (투명도 30%)
-                    // spreadRadius: 2, // 그림자 퍼지는 정도
-                    blurRadius: 15, // 그림자 흐림 정도
-                    offset: Offset(0, 10), // 그림자의 위치 (x, y)
-                  ),
-                ],
-              ),
-            ),
+                )),
+          ),
+        ),
+        SizedBox(height: 15),
+        Container(
+            decoration: myBoxDecoration,
+            child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: GestureDetector(
+                    onTap: () {
+                      Navigator.of(context)
+                          .push(SlidePageRoute(nextPage: ItemScreen()));
+                    },
+                    child: Row(
+                      mainAxisSize:
+                          MainAxisSize.min, // Row의 가로 길이를 내부 요소만큼만 차지하도록 설정
+                      children: const [
+                        Image(
+                          image: AssetImage('assets/icons/doghouse_random.png'),
+                          width: 30,
+                          height: 30,
+                          fit: BoxFit.cover,
+                          filterQuality: FilterQuality.none,
+                        ),
+                        SizedBox(width: 5),
+                        Text16(text: '뽑기')
+                      ],
+                    )))),
+      ],
     );
   }
 }
