4009da19713c624f93ec25529205ddeeca08de26
서민지 <s__min_ji@naver.com>
2024-05-16 16:44:54 +0900

    :sparkles: feat : 박스 위젯 추가

--- frontend/lib/widgets/box_dacoration.dart
+++ frontend/lib/widgets/box_dacoration.dart
@@ -23,6 +23,10 @@ final BoxDecoration myRedBoxDecoration = BoxDecoration(
     ),
   ],
 );
+final BoxDecoration myNoneRedBoxDecoration = BoxDecoration(
+  color: myLightRed, // 배경색
+  borderRadius: BorderRadius.circular(15), // 박스의 모서리를 둥글게
+);
 final BoxDecoration myYellowBoxDecoration = BoxDecoration(
   color: myLightYellow, // 배경색
   borderRadius: BorderRadius.circular(15), // 박스의 모서리를 둥글게
@@ -34,8 +38,23 @@ final BoxDecoration myYellowBoxDecoration = BoxDecoration(
     ),
   ],
 );
+final BoxDecoration myNoneYellowBoxDecoration = BoxDecoration(
+  color: myLightYellow, // 배경색
+  borderRadius: BorderRadius.circular(15), // 박스의 모서리를 둥글게
+);
 final BoxDecoration myGreenBoxDecoration = BoxDecoration(
   color: myWhiteGreen, // 배경색
+  borderRadius: BorderRadius.circular(15), // 박스의 모서리를 둥글게
+  // boxShadow: [
+  //   BoxShadow(
+  //     color: Color(0xffBFC2C8).withOpacity(0.25), // 그림자 색 (투명도 25%)
+  //     blurRadius: 15, // 그림자 흐림 정도
+  //     offset: Offset(0, 10), // 그림자의 위치 (x, y)
+  //   ),
+  // ],
+);
+final BoxDecoration myLightGreenBoxDecoration = BoxDecoration(
+  color: myLightGreen, // 배경색
   borderRadius: BorderRadius.circular(15), // 박스의 모서리를 둥글게
   boxShadow: [
     BoxShadow(
