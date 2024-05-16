7705b282490fbc9e8616a13432af224f700ee05b
서만기 <seo_m98@naver.com>
2024-05-17 02:30:32 +0900

    :sparkles: feat : 장착중 아이템 적용 및 강아지가 바뀌지 않는 버그 해결

--- frontend/lib/screens/doghouse/doghouse_screen.dart
+++ frontend/lib/screens/doghouse/doghouse_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:forsythia/models/inventory/my_item.dart';
 import 'package:forsythia/models/users/login_user.dart';
-import 'package:forsythia/screens/doghouse/move_dog.dart';
 import 'package:forsythia/screens/inventory/inventory_screen.dart';
 import 'package:forsythia/screens/item/item_screen.dart';
 import 'package:forsythia/service/inventory_service.dart';
@@ -19,7 +18,8 @@ class DogHouseScreen extends StatefulWidget {
   State<DogHouseScreen> createState() => _DogHouseScreenState();
 }
 
-class _DogHouseScreenState extends State<DogHouseScreen> {
+class _DogHouseScreenState extends State<DogHouseScreen>
+    with TickerProviderStateMixin {
   My my = My();
   int coin = 0;
   int affection = 0;
@@ -28,36 +28,48 @@ class _DogHouseScreenState extends State<DogHouseScreen> {
   bool active = false;
   Random random = Random();
   int randomNumber = 0;
+  // AnimationController를 생성
+  late AnimationController _controller;
+
+  // 현재 보여지는 이미지의 경로를 저장하기 위한 변수
+  late String currentImage;
+  // 이미지가 움직이는 중인지 여부를 나타내는 변수
+  List<double> dx = [10, 50, 100, 150, 200, 250];
+  List<double> dy = [110, 140, 210, 280, 350, 370];
+  int x = 0;
+  int y = 0;
+  int act = 0;
+  bool left = false;
 
   @override
   void initState() {
     super.initState();
+    // AnimationController를 초기화하고 지속시간을 500밀리초로 설정
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 500),
+    );
+    // 초기 이미지를 설정
     getItem();
     loadCoin();
+    act = random.nextInt(6);
+    x = random.nextInt(6);
+    y = random.nextInt(6);
   }
 
-  // List<List<String>> moveDog = [
-  //   ["assets/dogs/0_1.gif", "assets/dogs/1_1.gif"],
-  //   ["assets/dogs/0_2.gif", "assets/dogs/1_2.gif"],
-  //   ["assets/dogs/0_3.gif", "assets/dogs/1_3.gif"],
-  //   ["assets/dogs/0_4.gif", "assets/dogs/1_4.gif"],
-  //   ["assets/dogs/0_5.gif", "assets/dogs/1_5.gif"],
-  //   ["assets/dogs/0_6.gif", "assets/dogs/1_6.gif"],
-  //   ["assets/dogs/0_7.gif", "assets/dogs/1_7.gif"],
-  //   ["assets/dogs/0_8.gif", "assets/dogs/1_8.gif"],
-  //   ["assets/dogs/0_9.gif", "assets/dogs/1_9.gif"],
-  //   ["assets/dogs/0_10.gif", "assets/dogs/1_10.gif"],
-  // ];
-
-  // List<List<String>> actDog = [
-  //   ["assets/dogs/1_0_1.gif", "assets/dogs/1_1_1.gif", "assets/dogs/1_2_1.gif", "assets/dogs/1_3_1.gif", "assets/dogs/1_4_1.gif", "assets/dogs/1_5_1.gif"],
-  // ];
+  @override
+  void dispose() {
+    // AnimationController를 dispose하여 메모리 누수를 방지
+    _controller.dispose();
+    super.dispose();
+  }
 
   getItem() async {
     MyItem myItem = await InventoryService.fetchMyItem();
     setState(() {
       my = myItem.data!;
       active = true;
+      currentImage = "assets/dogs/1_${act}_${my.pet!.id!}.gif";
     });
   }
 
@@ -66,6 +78,40 @@ class _DogHouseScreenState extends State<DogHouseScreen> {
     LoginInfo? info = await storageService.getLoginInfo();
     setState(() {
       coin = info?.coin ?? 0;
+    });
+  }
+
+  int findIdByCategory(List<Items>? items, String category) {
+    for (var item in items!) {
+      if (item.category == category) {
+        return item.id!;
+      }
+    }
+    // 카테고리를 찾지 못했을 때 예외처리
+    throw Exception('카테고리를 찾을 수 없습니다: $category');
+  }
+
+  void moveImage() {
+    setState(() {
+      int a = x;
+      int b = y;
+      x = random.nextInt(6);
+      y = random.nextInt(6);
+      if ((a - x).abs() + (b - y).abs() > 4 || (b - y).abs() > 2) {
+        currentImage = "assets/dogs/1_${my.pet!.id!}.gif";
+      } else {
+        currentImage = "assets/dogs/0_${my.pet!.id!}.gif";
+      }
+      left = (a - x) < 0 ? false : true;
+    });
+
+    // AnimationController를 이용하여 위치 변경 애니메이션을 실행
+    _controller.reset();
+    _controller.forward().then((_) {
+      setState(() {
+        act = random.nextInt(6);
+        currentImage = "assets/dogs/1_${act}_${my.pet!.id!}.gif";
+      });
     });
   }
 
@@ -89,7 +135,7 @@ class _DogHouseScreenState extends State<DogHouseScreen> {
                     left: 0,
                     top: 0,
                     child: Image.asset(
-                      "assets/item/1.png",
+                      "assets/item/${findIdByCategory(my.items, "Wall")}.png",
                       width: MediaQuery.of(context).size.width,
                       filterQuality: FilterQuality.none,
                       fit: BoxFit.cover,
@@ -99,7 +145,7 @@ class _DogHouseScreenState extends State<DogHouseScreen> {
                     left: 0,
                     bottom: 0,
                     child: Image.asset(
-                      "assets/item/2.png",
+                      "assets/item/${findIdByCategory(my.items, "Floor")}.png",
                       width: MediaQuery.of(context).size.width,
                       filterQuality: FilterQuality.none,
                       fit: BoxFit.cover,
@@ -107,10 +153,30 @@ class _DogHouseScreenState extends State<DogHouseScreen> {
                   ),
                   Positioned(
                     left: 30,
-                    bottom: 300,
+                    bottom: 320,
                     child: Image.asset(
-                      "assets/item/3.png",
-                      width: 250,
+                      "assets/item/${findIdByCategory(my.items, "Rug")}.png",
+                      width: 350,
+                      filterQuality: FilterQuality.none,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Positioned(
+                    right: 60,
+                    bottom: 60,
+                    child: Image.asset(
+                      "assets/item/${findIdByCategory(my.items, "Bowl")}.png",
+                      width: 70,
+                      filterQuality: FilterQuality.none,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Positioned(
+                    left: 40,
+                    bottom: 120,
+                    child: Image.asset(
+                      "assets/item/${findIdByCategory(my.items, "Cushion")}.png",
+                      width: 200,
                       filterQuality: FilterQuality.none,
                       fit: BoxFit.cover,
                     ),
@@ -119,33 +185,43 @@ class _DogHouseScreenState extends State<DogHouseScreen> {
                     right: 40,
                     bottom: 200,
                     child: Image.asset(
-                      "assets/item/4.png",
+                      "assets/item/${findIdByCategory(my.items, "Toy")}.png",
                       width: 100,
                       filterQuality: FilterQuality.none,
                       fit: BoxFit.cover,
                     ),
                   ),
-                  Positioned(
-                    left: 40,
-                    bottom: 80,
-                    child: Image.asset(
-                      "assets/item/5.png",
-                      width: 200,
-                      filterQuality: FilterQuality.none,
-                      fit: BoxFit.cover,
+                  GestureDetector(
+                    // GestureDetector를 사용하여 이미지를 감싼다
+                    onTap: () {
+                      moveImage();
+                    },
+                    child: Stack(
+                      children: <Widget>[
+                        AnimatedPositioned(
+                          left: dx[x],
+                          top: dy[y],
+                          duration: Duration(milliseconds: 500),
+                          child: left
+                              ? Transform.flip(
+                                  flipX: true,
+                                  child: Image.asset(
+                                    currentImage,
+                                    width: 200.0 - (50 - y * 10),
+                                    fit: BoxFit.cover,
+                                    filterQuality: FilterQuality.none,
+                                  ),
+                                )
+                              : Image.asset(
+                                  currentImage,
+                                  width: 200.0 - (50 - y * 10),
+                                  fit: BoxFit.cover,
+                                  filterQuality: FilterQuality.none,
+                                ),
+                        ),
+                      ],
                     ),
                   ),
-                  Positioned(
-                    right: 40,
-                    bottom: 40,
-                    child: Image.asset(
-                      "assets/item/6.png",
-                      width: 80,
-                      filterQuality: FilterQuality.none,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  ImageMove(id: my.pet!.id!),
                   Padding(
                     padding: const EdgeInsets.all(10.0),
                     child: Column(
@@ -178,7 +254,14 @@ class _DogHouseScreenState extends State<DogHouseScreen> {
                 onTap: () {
                   Navigator.of(context)
                       .push(SlidePageRoute(nextPage: InventoryScreen()))
-                      .then(getItem());
+                      .then((result) {
+                    // 돌아올 때 데이터를 수신하고 처리
+                    if (result == "update") {
+                      // 리스트 업데이트 메서드 호출
+                      getItem();
+                      loadCoin();
+                    }
+                  });
                 },
                 child: Row(
                   mainAxisSize:
@@ -206,7 +289,15 @@ class _DogHouseScreenState extends State<DogHouseScreen> {
                     onTap: () {
                       Navigator.of(context)
                           .push(SlidePageRoute(nextPage: ItemScreen()))
-                          .then(getItem());
+                          .then((result) {
+                        // 돌아올 때 데이터를 수신하고 처리
+                        if (result == "update") {
+                          // 리스트 업데이트 메서드 호출
+                          getItem();
+                          loadCoin();
+                          print("sddsfsdf");
+                        }
+                      });
                     },
                     child: Row(
                       mainAxisSize:
