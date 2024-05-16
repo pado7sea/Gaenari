90f5a25fac5eac110c14cd61d3b9382e8e0804bb
이재신 <jaesin463@gmail.com>
2024-05-16 15:41:55 +0900

    :wrench: refact : memberId -> accountId

--- fcm-service/src/main/java/com/gaenari/backend/controller/FcmController.java
+++ fcm-service/src/main/java/com/gaenari/backend/controller/FcmController.java
@@ -5,6 +5,7 @@ import com.gaenari.backend.dto.FcmRegisterRequestDto;
 import com.gaenari.backend.service.FcmService;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -23,20 +24,26 @@ public class FcmController {
 
   private final FcmService fcmService;
 
-  @Operation(summary = "[Feign] Fcm 토큰 저장", description = "Fcm 토큰 정보 저장")
+  @Operation(summary = "Fcm 토큰 저장", description = "Fcm 토큰 정보 저장")
   @PostMapping("/save")
-  @ApiResponse(responseCode = "200", description = "[Feign] Fcm 토큰 저장 성공")
-  public ResponseEntity<?> saveToken(@RequestBody FcmRegisterRequestDto requestDto){
+  @ApiResponse(responseCode = "200", description = "Fcm 토큰 저장 성공")
+  public ResponseEntity<?> saveToken(
+      @Parameter(hidden = true) @RequestHeader("User-Info") String accountId,
+      @RequestParam String fcmToken) {
+    FcmRegisterRequestDto requestDto = FcmRegisterRequestDto.builder()
+        .accountId(accountId)
+        .fcmToken(fcmToken)
+        .build();
+
     fcmService.register(requestDto);
     return new ResponseEntity<>(HttpStatus.OK);
   }
 
   @Operation(summary = "[Feign] 개인 메세지 전송", description = "Fcm을 활용한 개인 메세지 전송")
-  @PostMapping("/send")
-  @ApiResponses(value = {
-      @ApiResponse(responseCode = "200", description = "[Feign] 메세지 전송 성공", content = @Content(schema = @Schema(implementation = String.class)))
-  })
-  public ResponseEntity<?> sendMessage(@RequestBody FcmMessageRequestDto requestDto) throws FirebaseMessagingException{
+  @PostMapping("/feign/send")
+  @ApiResponse(responseCode = "200", description = "[Feign] 메세지 전송 성공")
+  public ResponseEntity<?> sendMessage(@RequestBody FcmMessageRequestDto requestDto)
+      throws FirebaseMessagingException {
     fcmService.sendNotice(requestDto);
     return new ResponseEntity<>(HttpStatus.OK);
   }
