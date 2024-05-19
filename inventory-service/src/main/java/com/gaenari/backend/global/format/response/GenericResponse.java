cdf2d541f332384f758dc1cc936891105cb15e9c
최다희 <dahee121816@gmail.com>
2024-05-19 16:37:26 +0900

    :sparkles: feat : FeignClint 써킷브레이커 추가

--- inventory-service/src/main/java/com/gaenari/backend/global/format/response/GenericResponse.java
+++ inventory-service/src/main/java/com/gaenari/backend/global/format/response/GenericResponse.java
@@ -1,8 +1,10 @@
 package com.gaenari.backend.global.format.response;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class GenericResponse<T> {
     private String status;
     private String message;
